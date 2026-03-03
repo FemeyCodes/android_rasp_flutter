fa7be2c08f0cede856711cab2d21e4876a8915e4
timfemey <isholaobafemi@gmail.com>
2026-03-03 20:16:26 +0100

    AndroidRaspFlutterPlugin full channel calls from Kotlin

--- android/src/main/kotlin/com/androidraspflutter/android_rasp_flutter/AndroidRaspFlutterPlugin.kt
+++ android/src/main/kotlin/com/androidraspflutter/android_rasp_flutter/AndroidRaspFlutterPlugin.kt
@@ -1,38 +1,94 @@
 package com.androidraspflutter.android_rasp_flutter
 
+import android.content.Context
+import android.os.Handler
+import android.os.Looper
+import androidx.annotation.NonNull
 import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.plugin.common.EventChannel
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
-import io.flutter.plugin.common.MethodChannel.Result
+import io.flutter.plugin.common.MethodChannel.Result as FlutterResult
 
-/** AndroidRaspFlutterPlugin */
-class AndroidRaspFlutterPlugin :
-    FlutterPlugin,
-    MethodCallHandler {
-    // The MethodChannel that will the communication between Flutter and native Android
-    //
-    // This local reference serves to register the plugin with the Flutter Engine and unregister it
-    // when the Flutter Engine is detached from the Activity
-    private lateinit var channel: MethodChannel
+import com.securevale.rasp.android.SecureApp
+import com.securevale.rasp.android.api.SecureAppChecker
+import com.securevale.rasp.android.api.result.Result as RaspResult
 
-    override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-        channel = MethodChannel(flutterPluginBinding.binaryMessenger, "android_rasp_flutter")
-        channel.setMethodCallHandler(this)
+class AndroidRaspFlutterPlugin : FlutterPlugin, MethodCallHandler, EventChannel.StreamHandler {
+
+    private lateinit var methodChannel: MethodChannel
+    private lateinit var eventChannel: EventChannel
+    private lateinit var context: Context
+
+    private var checker: SecureAppChecker? = null
+    private var eventSink: EventChannel.EventSink? = null
+
+    private val mainHandler = Handler(Looper.getMainLooper())
+
+    override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+        context = flutterPluginBinding.applicationContext
+
+        SecureApp.init()
+
+        checker = SecureAppChecker.Builder(
+            context,
+            checkEmulator = true,
+            checkDebugger = true,
+            checkRoot = true
+        ).build()
+
+        methodChannel = MethodChannel(flutterPluginBinding.binaryMessenger, "android_rasp_plugin")
+        methodChannel.setMethodCallHandler(this)
+
+        eventChannel = EventChannel(flutterPluginBinding.binaryMessenger, "android_rasp_plugin/events")
+        eventChannel.setStreamHandler(this)
     }
 
-    override fun onMethodCall(
-        call: MethodCall,
-        result: Result
-    ) {
-        if (call.method == "getPlatformVersion") {
-            result.success("Android ${android.os.Build.VERSION.RELEASE}")
-        } else {
-            result.notImplemented()
+    override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: FlutterResult) {
+        when (call.method) {
+            "getPlatformVersion" -> {
+                result.success("Android ${android.os.Build.VERSION.RELEASE}")
+            }
+            "getSecurityStatus" -> {
+               
+                val checkResult = checker?.check()
+                result.success(mapResultToString(checkResult))
+            }
+            else -> result.notImplemented()
         }
     }
 
-    override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
-        channel.setMethodCallHandler(null)
+   
+    override fun onListen(arguments: Any?, events: EventChannel.EventSink?) {
+        eventSink = events
+        Thread {
+            try {
+                val checkResult = checker?.check()
+                val resultString = mapResultToString(checkResult)
+                mainHandler.post { eventSink?.success(resultString) }
+            } catch (e: Exception) {
+                mainHandler.post { eventSink?.error("RASP_ERROR", e.message, null) }
+            }
+        }.start()
+    }
+
+    override fun onCancel(arguments: Any?) {
+        eventSink = null
+    }
+
+    private fun mapResultToString(result: RaspResult?): String {
+        return when (result) {
+            is RaspResult.Secure -> "secure"
+            is RaspResult.Rooted -> "rooted"
+            is RaspResult.EmulatorFound -> "emulator"
+            is RaspResult.DebuggerEnabled -> "debugger"
+            else -> "unknown"
+        }
+    }
+
+    override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+        methodChannel.setMethodCallHandler(null)
+        eventChannel.setStreamHandler(null)
     }
 }
